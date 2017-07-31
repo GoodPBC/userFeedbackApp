b54a4ec331be11c462864718c1240c821bfde5ae
Joseph Pulaski <jpulaski79@gmail.com>
2017-07-31 17:41:30 -0400

    created Google passport strategy and google auth get route with callback and scope

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const express = require('express');
 const passport = require('passport');
 //Google Passport Strategy
 const GoogleAuth = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
 //generate express app
 const app = express();
 
 //instance of passport google stategy
-passport.use(new GoogleAuth());
+passport.use(
+  new GoogleAuth(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.lgo(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 //In production run on env var port || or run on port 5000
 const PORT = process.env.PORT || 5000;
