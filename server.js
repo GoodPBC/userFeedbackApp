f70ff5a72fa4ce32d836c709f047c17de91c225d
GoodPBC <admin@goodygiver.com>
2017-08-03 08:58:03 -0400

    reafcactored passport and routes away from server. Now requiring routes as a function to server.js

--- server.js
+++ server.js
@@ -1,47 +1,17 @@
-//import dependencies
+//import express
 const express = require('express');
-//passport auth
-const passport = require('passport');
-//Google Passport Strategy
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//require passportJS
+require('./services/passport');
 
-const keys = require('./config/keys');
+const authRoutes = require('./routes/auth');
 //generate express app
 const app = express();
 
-// Google Stategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID, // keys
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback' //callback URL
-    },
-    //this gets executed after the callback. Save access token to DB, it is our key to each user.
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('Display Name', profile.displayName);
-    }
-  )
-);
+authRoutes(app);
 
-/* ROUTES*/
-
-//google auth route
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-//google auth callback
-app.get('/auth/google/callback',
-  passport.authenticate('google', {})
-);
 
 //In production run on env var port || or run on port 5000
 const PORT = process.env.PORT || 5000;
+
 //express tells node to listen on a Port
 app.listen(PORT);
