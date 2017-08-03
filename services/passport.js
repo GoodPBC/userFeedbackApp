ce7259a93a8ad90582a32d353abeb91159e0cca8
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-03 13:52:43 -0400

    create new IG auth instance

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,8 @@
 const passport = require('passport');
 //Google Passport Strategy
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//Instagram Passport Strategy
+const InstagramStrategy = require('passport-instagram');
 //google keys
 const keys = require('../config/keys');
 
@@ -17,7 +19,23 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       console.log('access token', accessToken);
       console.log('refresh token', refreshToken);
-      console.log('Display Name', profile.displayName);
+      console.log('Display Name', profile);
+    }
+  )
+);
+
+//Instagram Passport Strategy
+passport.use(
+  new InstagramStrategy(
+    {
+      clientID: keys.INSTAGRAM_CLIENT_ID,
+      clientSecret: keys.INSTAGRAM_CLIENT_SECRET,
+      callbackURL: 'http://127.0.0.1:3000/auth/instagram/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ instagramId: profile.id }, function(err, user) {
+        return done(err, user);
+      });
     }
   )
 );
