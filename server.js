620991dc50b1049b4a507b603091ddb14d7a8d4d
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-04 09:57:10 -0400

    mongo database connection working

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 //import express
 const express = require('express');
+//require mongoose
+const mongoose = require('mongoose');
+//require config file
+const keys = require('./config/keys');
 //require passportJS
 require('./services/passport');
 
+//mongo connection
+mongoose.connect(keys.mongoURI);
 
 //generate express app
 const app = express();
 
 //require auth routes as function and call function on (app)
 require('./routes/auth')(app);
-
 
 //In production run on env var port || or run on port 5000
 const PORT = process.env.PORT || 5000;
