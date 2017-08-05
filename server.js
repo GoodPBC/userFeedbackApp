cbd06f57dd1e94208585ed278406763dfbbfbad6
GoodPBC <admin@goodygiver.com>
2017-08-05 12:40:58 -0400

    moved mongoose require statement

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 //import express
 const express = require('express');
-
-//require mongoose UserSchema
-require('./models/User');
 //require mongoose
 const mongoose = require('mongoose');
 //require config file
 const keys = require('./config/keys');
 //require passportJS
 require('./services/passport');
+//require mongoose UserSchema
+require('./models/User');
 
 //mongo connection
 mongoose.connect(keys.mongoURI);
@@ -21,6 +20,5 @@ require('./routes/auth')(app);
 
 //In production run on env var port || or run on port 5000
 const PORT = process.env.PORT || 5000;
-
 //express tells node to listen on a Port
 app.listen(PORT);
