45ae8974c340e1bc5866046fa8f668882403ff48
GoodPBC <admin@goodygiver.com>
2017-08-05 12:39:31 -0400

    create mongoose user schema

---
+++ models/User.js
@@ -0,0 +1,12 @@
+//require mongoose
+const mongoose = require('mongoose');
+//import the schema property from the mongoose object
+const Schema  = mongoose.Schema;
+
+//create new user schema
+const userSchema = new Schema ({
+  googleId: String,
+  instagramId: String
+});
+
+mongoose.model('Users', userSchema);
