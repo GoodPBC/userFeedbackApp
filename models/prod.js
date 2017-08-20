9009a6590b798add93a8038a273cbee52e3616ec
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-20 15:03:04 -0400

    removed accidental duplicate file

--- models/prod.js
+++
@@ -1,9 +0,0 @@
-//production keys will go here
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  INSTAGRAM_CLIENT_ID: process.env.INSTAGRAM_CLIENT_ID,
-  INSTAGRAM_CLIENT_SECRET: process.env.INSTAGRAM_CLIENT_ID,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
-};
