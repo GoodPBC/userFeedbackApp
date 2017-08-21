5436e1c916d4f3c7caedded8ff44a07e24ccd6f7
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 12:20:21 -0400

    Refactor #1  (remove return) - refactor action creator for async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 //redux action creator
-export const fetchUser = () => {
+export const fetchUser = () =>
   //using dispatch function from thunk
-  return function(dispatch) {
+  function(dispatch) {
     axios
       .get('/api/current_user')
       .then(res => dispatch({ type: FETCH_USER, payload: res }));
   };
-};
