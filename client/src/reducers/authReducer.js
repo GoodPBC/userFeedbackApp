defff1ad08b988a2910f101baca80fc4214c475c
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 16:06:26 -0400

    Request current user from server returns null --> user logged in???

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,12 @@
 import { FETCH_USER } from '../actions/types';
 
 //auth reducer function
-export default function(state = {}, action) {
+export default function(state = null, action) {
   console.log(action);
   switch (action.type) {
     case FETCH_USER:
-
+      //action.payload = User model
+      return action.payload;
     default:
       return state;
   }
