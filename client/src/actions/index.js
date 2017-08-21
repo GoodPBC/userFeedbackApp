55d05f142fef7b03bcd2c6f9868bdcebc5cb7560
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 15:20:41 -0400

    Refactor #4 (combine fnctn) - refactor action creator for async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 //redux action creator using dispatch function from thunk
-export const fetchUser = () => dispatch => {
-  axios
-    .get('/api/current_user')
-    .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//return a function that makes a request. When req completed, dispatch action
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res });
 };
