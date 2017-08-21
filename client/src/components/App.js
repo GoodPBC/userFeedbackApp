c1ae931a8e16a8236dfca0b7c648881a5ed20dc2
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-20 23:35:53 -0400

    created dummy components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const CreateLoor = () => <h2>Create Loor</h2>;
+const ViewLoor = () => <h2>View Loor</h2>;
+const Analytics = () => <h2>Analytics</h2>;
+const Settings = () => <h2>Settings</h2>;
+
 const App = () => {
   return <div>Hi there!</div>;
 };
