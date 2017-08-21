a9792e355925c5c76110fe4f157877791ae59fa2
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-20 23:45:19 -0400

    add landing and dashboard routes with exact logic for react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const CreateLoor = () => <h2>Create Loor</h2>;
 const ViewLoor = () => <h2>View Loor</h2>;
@@ -10,7 +11,16 @@ const Analytics = () => <h2>Analytics</h2>;
 const Settings = () => <h2>Settings</h2>;
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact={true} component={Landing} />
+          <Route path="/dashboard" exact={true} component={Dashboard} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
