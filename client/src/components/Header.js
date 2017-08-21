d70e3c0c358305e1045e3d4731591c29129cec9a
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 00:41:14 -0400

    added navbar and began style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <nav>
-        <div className="nav-Wrapper" />
+      <nav className="yellow darken-3">
+        <div className="nav-Wrapper">
+          <a className="left brand-logo" href="/">
+            Loor
+          </a>
+          <ul className="right">
+            <li>
+              <a className="black-text">Login with instagram</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
