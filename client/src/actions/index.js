3a297a0fd81d0e1ad793e2badc083f2346afd1e0
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 12:22:16 -0400

    Refactor #2  (fat arrow) - refactor action creator for async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from './types';
 //redux action creator
 export const fetchUser = () =>
   //using dispatch function from thunk
-  function(dispatch) {
+  dispatch => {
     axios
       .get('/api/current_user')
       .then(res => dispatch({ type: FETCH_USER, payload: res }));
