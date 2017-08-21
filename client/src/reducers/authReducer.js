b2df9f3755c2016408822b4b78f5a1d3f9f0a175
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 16:14:58 -0400

    remove logging

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,7 +2,7 @@ import { FETCH_USER } from '../actions/types';
 
 //auth reducer function
 export default function(state = null, action) {
-  console.log(action);
+  // console.log(action);
   switch (action.type) {
     case FETCH_USER:
       //action.payload = User model
