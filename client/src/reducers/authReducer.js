4a3b34641573e17c6b6308146e6e57b132e0ef53
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-21 16:01:00 -0400

    import FETCH_USER action type to auth reducer and setup switch case to whatch action come to reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,11 @@
+import { FETCH_USER } from '../actions/types';
+
 //auth reducer function
 export default function(state = {}, action) {
   console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+
     default:
       return state;
   }
