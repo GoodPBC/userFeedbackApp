ed549d02ea212f429bd0bc8f6439f3b79e2cb145
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-26 00:33:19 -0400

    making http request to logout route

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
       default:
         return (
           <li>
-            <a>Log out</a>
+            <a href="/api/logout">Log out</a>
           </li>
         );
     }
