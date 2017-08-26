2247e50126a2f86ba6ffdc2799553a7284a3201c
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-26 11:49:00 -0400

    created Footer component for client

---
+++ client/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer page-footer yellow darken-3">
+      <div className="container">
+        <div className="loor-llc"> 2016 All Rights Reserved. LOOR LLC </div>
+        <div className="footer-links">
+          <div className="link-item">info@loor.life</div>
+        </div>
+        <div className="footer-social-networks">
+          <i
+            className="fa fa-facebook footer-social-icons"
+            aria-hidden="true"
+          />
+          <i className="fa fa-twitter footer-social-icons" aria-hidden="true" />
+          <i
+            className="fa fa-instagram footer-social-icons"
+            aria-hidden="true"
+          />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
