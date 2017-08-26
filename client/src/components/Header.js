65ce177d2f2c25dcfebbf66d44fa290d8ddd9a84
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-26 17:11:42 -0400

    Make Logo auth aware & w/react router & w/ Link tags. Link from react-router-dom

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; //import connect helper
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContentHelper() {
