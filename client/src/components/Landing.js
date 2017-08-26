7a1f621b708b428621ec6c1a3de80004560509f0
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-26 00:44:14 -0400

    create funcitonal Landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style{{textAlign: 'center'}}>
+      <h1>Loor</h1>
+      We Dont Believe in product placement, We believe in People Placement!
+    </div>
+  );
+};
+
+export default Landing;
