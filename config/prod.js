4b7674878b64536afe376b4444cc5de2b509a5af
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-26 19:04:36 -0400

    create ENV VAR for stripe.js

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,7 @@ module.exports = {
   INSTAGRAM_CLIENT_ID: process.env.INSTAGRAM_CLIENT_ID,
   INSTAGRAM_CLIENT_SECRET: process.env.INSTAGRAM_CLIENT_ID,
   mongoURI: process.env.MONGODB_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
