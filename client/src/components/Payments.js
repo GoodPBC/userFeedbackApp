3497999412a3e26efdc2cee982bcec25b2a1a550
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-26 22:43:02 -0400

    removed debugger

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,8 +3,6 @@ import ReactStripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render() {
-    debugger;
-
     return (
       <ReactStripeCheckout
         amount={100}
