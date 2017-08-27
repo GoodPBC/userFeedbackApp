acbc4f291932bfd47d231cc8bbbe66ab24467b40
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-27 12:26:59 -0400

    Change payment component Name and Description

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,6 +5,8 @@ class Payments extends Component {
   render() {
     return (
       <ReactStripeCheckout
+        name="Loor"
+        description="25 Loors for $5.00"
         amount={100}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
