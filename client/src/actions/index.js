dcbed9e5cee62760213a97ba131c73f67e47b160
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-28 15:09:40 -0400

    action creator to send stripe token to server api route

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,3 +8,8 @@ export const fetchUser = () => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+//call sendStripeTokenToServer with token from  stripe and call async function with dispatch method
+export const sendStripeTokenToServer = token => async dispatch => {
+  //make post request to backend server to send token
+  const res = await axios.post('/api/payment_token', token);
+};
