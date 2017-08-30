d46e2953b8c68877c055c7a7722565915f2411e5
Joseph Pulaski <jpulaski79@gmail.com>
2017-08-30 15:39:09 -0400

    add credits to user Model

--- models/User.js
+++ models/User.js
@@ -1,17 +1,18 @@
 //require mongoose
 const mongoose = require('mongoose');
 //import the schema property from the mongoose object
-const Schema  = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 //create new user schema
-const userSchema = new Schema ({
+const userSchema = new Schema({
   googleId: String,
   instagramId: String,
   displayName: String,
   name: {
     first: String,
     last: String
-  }
+  },
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('User', userSchema);
