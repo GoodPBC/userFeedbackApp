010afff2bbf99a2e1a9226e97f7f8fc6fb392daf
Joseph Pulaski <jpulaski79@gmail.com>
2017-09-03 16:15:27 -0400

    Major Heroku production refactor - auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,6 @@
-//import express
-const express = require('express');
-//imoport passportJS npm module
 const passport = require('passport');
-/* AUTHENTICATION ROUTES*/
 
-//export google auth routes to
 module.exports = app => {
-  // app.get('/dashboard', (req, res) => {
-  //   res.send('This is is the dashboard');
-  // });
-
-  //google auth route
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -18,35 +8,30 @@ module.exports = app => {
     })
   );
 
-  //google auth callback route
   app.get(
     '/auth/google/callback',
     passport.authenticate('google'),
     (req, res) => {
-      res.redirect('/dashboard');
+      res.redirect('/surveys');
     }
   );
 
-  //IG
-  //ig auth route
   app.get('/auth/instagram', passport.authenticate('instagram'));
 
   app.get(
     '/auth/instagram/callback',
-    passport.authenticate('instagram'),
-    (req, res) => {
+    passport.authenticate('instagram', { failureRedirect: '/' }),
+    function(req, res) {
       // Successful authentication, redirect home.
-      res.redirect('/dashboard');
+      res.redirect('/surveys');
     }
   );
 
-  //log out
   app.get('/api/logout', (req, res) => {
     req.logout();
     res.redirect('/');
   });
 
-  //API Route to return user object for logged in user
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
